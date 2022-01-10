c8af6eb837f288759857b224a0c41d8f5d74357c
Bdani2201 <bdani2201@gmail.com>
2022-01-10 11:50:11 +0100

    Add project files.

---
+++ 2022-01-10_Snooker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2022_01_10_Snooker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
